9a6a3c8e31a7d5f886a1524057b7375e1251ea15
rebollogeoffrey <geoffrey.rebollo@free.fr>
2023-07-25 10:08:48 +0200

    getPersonType Done

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,11 @@ class ComplimentMachine<T> {
     return "";
   }
 
-  private getPersonType() {}
+  private getPersonType(person: T) {
+    if (typeof person === "string") return "ami.e";
+    else if (typeof person === "number") return "collegue";
+    else if (typeof person === "boolean") return "membre de la famille";
+    else return "Le type de personne n'est pas connu";
+  }
   private getComplimentForPersonType() {}
 }
