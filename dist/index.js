a164f48007879637d7cdf3e90746a658df0348a7
rebollogeoffrey <geoffrey.rebollo@free.fr>
2023-07-25 10:00:52 +0200

    init

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const complimentMachine_1 = require("./class/complimentMachine");
-const test = new complimentMachine_1.ComplimentMachine(true, "gentil");
-console.log('test.compliment(2) :>> ', test.compliment());
-const test2 = new complimentMachine_1.ComplimentMachine(2, "drol");
-console.log('test.compliment(2) :>> ', test2.compliment());
-const test3 = new complimentMachine_1.ComplimentMachine("Bob", "gentil");
-console.log('test.compliment(2) :>> ', test3.compliment());
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,8 @@
-import {ComplimentMachine} from "./class/complimentMachine"
+class ComplimentMachine<T> {
+  compliment(person: T): string {
+    return "";
+  }
 
-const test = new ComplimentMachine<boolean>(true, "gentil")
-
-console.log('test.compliment(2) :>> ', test.compliment());
-
-const test2 = new ComplimentMachine<number>(2, "drole")
-
-console.log('test.compliment(2) :>> ', test2.compliment());
-
-const test3= new ComplimentMachine<string>("Bob", "gentil")
-
-console.log('test.compliment(2) :>> ', test3.compliment());
+  private getPersonType() {}
+  private getComplimentForPersonType() {}
+}
